6474564d08fb9219499d596efca99959ac3c0442
aelt95 <anthonylopez1230@gmail.com>
2023-05-24 12:46:16 +0200

    added location

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,14 @@
 // import Header from "../components/header.jsx";
 import Header from "@/components/header.jsx";
 import AboutUs from "@/components/about-us";
+import Location from "@/components/location";
 
 export default function Home() {
   return (
     <>
       <Header></Header>
       <AboutUs></AboutUs>
+      <Location />
     </>
   );
 }
